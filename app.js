366134724c894ea7caf0f524c01128990ab34ad4
phamsongluong <psluong952@gmail.com>
2023-02-08 21:24:42 +0700

    8.2.2023

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First middle ware');
+app.use('/', (req, res, next) => {
+    console.log('ss');
     next();
 });
 
-app.use((req, res, next) => {
-    console.log('Second middle ware');
+app.use('/add-product', (req, res, next) => {
+    console.log('First middle ware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>');
+});
+
+app.use('/product', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('Second middleware');
+    res.send('<h1>Main Page</h1>');
 });
 
 app.listen(3003);
