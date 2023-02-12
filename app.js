48e007c1a8f0de39ba2e63b9f8a356cea8f73582
phamsongluong <psluong952@gmail.com>
2023-02-12 10:58:52 +0700

    End section 5

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const path = require('path');
 
-app.use('/', (req, res, next) => {
-    console.log('ss');
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/add-product', (req, res, next) => {
-    console.log('First middle ware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/product', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    console.log('Second middleware');
-    res.send('<h1>Main Page</h1>');
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3003);
